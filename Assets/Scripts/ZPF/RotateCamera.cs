cf4f64bd83cd1505dcfe0b916959e70e199c6454
PhilipZeratul <p.f.zeratul@gmail.com>
2016-08-19 16:29:25 +0800

    modified rotatecamera

--- Assets/Scripts/ZPF/RotateCamera.cs
+++ Assets/Scripts/ZPF/RotateCamera.cs
@@ -1,51 +1,53 @@
-﻿using UnityEngine;
-using System.Collections;
-using OpenCVForUnity;
+﻿using OpenCVForUnity;
 using System.Collections.Generic;
 
-public class RotateCamera {
+namespace MagicCircuit
+{
+    public class RotateCamera
+    {
 
-	List<Point> ptsBoard;
-	List<Point> ptswindow;
+        List<Point> ptsBoard;
+        List<Point> ptsWindow;
 
-	Mat homo;
+        Mat homo;
 
-	public RotateCamera()
-	{
-		ptsBoard = new List<Point>();
-		ptswindow = new List<Point>();
+        public RotateCamera()
+        {
+            ptsBoard = new List<Point>();
+            ptsWindow = new List<Point>();
 
-		ptsBoard.Add(new Point(60, 123));
-		ptsBoard.Add(new Point(398, 125));
-		ptsBoard.Add(new Point(119, 411));
-		ptsBoard.Add(new Point(337, 411));
+            ptsBoard.Add(new Point(60, 123));
+            ptsBoard.Add(new Point(398, 125));
+            ptsBoard.Add(new Point(119, 411));
+            ptsBoard.Add(new Point(337, 411));
 
-		ptswindow.Add(new Point(7, 28));
-		ptswindow.Add(new Point(478, 28));
-		ptswindow.Add(new Point(7, 570));
-		ptswindow.Add(new Point(478, 570));
+            ptsWindow.Add(new Point(7, 28));
+            ptsWindow.Add(new Point(478, 28));
+            ptsWindow.Add(new Point(7, 570));
+            ptsWindow.Add(new Point(478, 570));
 
-		Mat rectBrd = Converters.vector_Point2f_to_Mat(ptsBoard);
-		Mat rectWin = Converters.vector_Point2f_to_Mat(ptswindow);
+            Mat rectBrd = Converters.vector_Point2f_to_Mat(ptsBoard);
+            Mat rectWin = Converters.vector_Point2f_to_Mat(ptsWindow);
 
-		homo = Imgproc.getPerspectiveTransform(rectBrd, rectWin);
-	}
+            homo = Imgproc.getPerspectiveTransform(rectBrd, rectWin);
+        }
 
-	public void rotate(ref Mat img)
-	{
-		Core.transpose(img, img);
+        public void rotate(ref Mat img)
+        {
+            Core.transpose(img, img);
 
-		Mat tmp = transform(img);
+            Mat tmp = transform(img);
 
-		img = tmp.clone();
-	}
+            img = new Mat(tmp, new Rect(ptsWindow[0], ptsWindow[3]));
+        }
 
-	public Mat transform(Mat img)
-	{        
-		Mat rst = new Mat();
+        public Mat transform(Mat img)
+        {
+            Mat rst = new Mat();
 
-		Imgproc.warpPerspective(img, rst, homo, img.size());
+            Imgproc.warpPerspective(img, rst, homo, img.size());
 
-		return rst;
-	}
+            return rst;
+        }
+    }
 }
