4118d65e7c24795f5ee9e30dff754c48379ff0c8
Zeratul Phillip <ZhangPengfei@Zeratuls-Mac-mini.local>
2016-11-07 14:49:42 +0800

    fixed conflict

--- Assets/Scripts/WQ/LevelSpecial/LevelThree.cs
+++ Assets/Scripts/WQ/LevelSpecial/LevelThree.cs
@@ -36,39 +36,9 @@ public class LevelThree : MonoBehaviour
 				}
 				isTest=false;
 			} 
-<<<<<<< HEAD
-			GetImage._instance.cf.switchOnOff (int.Parse (normalSwitch.tag), normalSwitch.GetComponent<SwitchCtrl> ().isSwitchOn ? false : true);
-
-
-			for (int i = 0; i < GetImage._instance.itemList.Count; i++)
-			{
-				Debug.Log("LevelThree.cs Update : itemList[" + i + "].type = " + GetImage._instance.itemList[i].type +
-					" powered = " + GetImage._instance.itemList[i].powered);
-			}
-
-//			//for test  ....
-//			if (!normalSwitch.GetComponent<SwitchCtrl> ().isSwitchOn && !isTest) 
-//			{
-//				for (int i = 0; i < PhotoRecognizingPanel._instance.itemList.Count; i++) 
-//				{
-//					if (PhotoRecognizingPanel._instance.itemList[i].type==ItemType.CircuitLine) 
-//					{
-//						//Debug.Log("itemlist["+i+"] powered: "+PhotoRecognizingPanel._instance.itemList[i].powered);
-//						
-//					}
-//				}
-//				isTest=true;
-//
-//			}
-
-
-
-			CommonFuncManager._instance.CircuitReset (	GetImage._instance.itemList);
-=======
 //			GetImage._instance.cf.switchOnOff (int.Parse (normalSwitch.tag), normalSwitch.GetComponent<SwitchCtrl> ().isSwitchOn ? false : true);
 //			CommonFuncManager._instance.CircuitReset (	GetImage._instance.itemList);
 			CommonFuncManager._instance.ArrowsRefresh(GetImage._instance.itemList);
->>>>>>> 4426aa6258570985d3679e3fef3cd072bd1906eb
 		}
 	}
 
